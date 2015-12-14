916346264bbbc63875ee22108e3618b82a818faa
Yoshua Wuyts <i@yoshuawuyts.com>
2015-12-15 07:29:31 +1100

    .

---
+++ app-main/index.js
@@ -0,0 +1,37 @@
+const boleStream = require('bole-stream')
+const httpNdjson = require('http-ndjson')
+const sizeStream = require('size-stream')
+const summary = require('server-summary')
+const stdout = require('stdout-stream')
+const pumpify = require('pumpify')
+const bole = require('bole')
+const http = require('http')
+
+module.exports = createServer
+
+function createServer (argv) {
+  bole.output({ level: argv.logLevel, stream: stdout })
+  const logStream = boleStream({ level: argv.logLevel })
+  const port = argv.port
+
+  // create server
+  const server = http.createServer((req, res) => {
+    const httpLogger = httpNdjson(req, res)
+    httpLogger.pipe(logStream, { end: false })
+
+    const size = sizeStream()
+    size.once('size', size => httpLogger.setContentLength(size))
+
+    const sink = pumpify(size, res)
+
+    console.log(req.headers.cookie)
+    res.setHeader('Set-Cookie', [
+      'type=ninja',
+      'language=javascript'
+    ])
+    sink.end('hello world')
+  })
+
+  // start the server on port
+  server.listen(port, summary(server))
+}
